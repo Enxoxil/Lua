4f4ed3d3127d1365ecf137cf013ac7728e67dce4
Yehor Haponov <egoriks1994@gmail.com>
2024-11-19 21:06:52 +0200

    Change input parameter freq instead of band/channel

--- change_vtx_channel/change_vtx_channel.lua
+++ change_vtx_channel/change_vtx_channel.lua
@@ -10,20 +10,6 @@
 -- @end
 -- 
 
-
-
-local frequency_map = {
-	-- Band X = 8 
-	[4990] = { 8, 0 }, [5020] = { 8, 1 }, [5050] = { 8, 2 },
-	[5080] = { 8, 3 }, [5110] = { 8, 4 }, [5140] = { 8, 5 },
-	[5170] = { 8, 6 }, [5200] = { 8, 7 },
-
-	-- Band R = 4
-	[5658] = { 4, 0 }, [5695] = { 4, 1 }, [5732] = { 4, 2 },
-	[5769] = { 4, 3 }, [5806] = { 4, 4 }, [5843] = { 4, 5 },
-	[5880] = { 4, 6 }, [5917] = { 4, 7 },
-};
-
 local vtx_freq_for_first_value_rc = nil;
 local vtx_freq_for_second_value_rc = nil;
 local vtx_freq_for_third_value_rc = nil;
@@ -32,6 +18,7 @@ local vtx_freq_for_fifth_value_rc = nil;
 
 local aux_for_change_VTX_channel = nil;
 local RC_state = nil;
+local button_counter = 0;
 
 local function initialize_parameters()
 	-- get parameters
@@ -56,10 +43,8 @@ local function get_vtx_channel(freq, map)
 	return map[freq];
 end;
 
-local function set_vtx_channel(band, channel, freq)
-	if band and channel then
-		-- param:set('VTX_BAND', band);
-		-- param:set('VTX_CHANNEL', channel);
+local function set_vtx_channel(freq)
+	if freq then
 		param:set('VTX_FREQ', freq);
 	else
 		gcs:send_text(0, "Error: band or channel is nil");
@@ -67,28 +52,24 @@ local function set_vtx_channel(band, channel, freq)
 end;
 
 function update()
-	local data = nil;
 	local state_switch_pwm = rc:get_pwm(aux_for_change_VTX_channel);
 	local current_freq = nil;
 
 	if (RC_state ~= state_switch_pwm) then
 		if state_switch_pwm > 800 and state_switch_pwm < 1301 then
-		data = get_vtx_channel(vtx_freq_for_first_value_rc, frequency_map);
-		current_freq = vtx_freq_for_first_value_rc;
-		elseif state_switch_pwm > 1300 and state_switch_pwm < 1801 then
-		data = get_vtx_channel(vtx_freq_for_second_value_rc, frequency_map);
-		current_freq = vtx_freq_for_second_value_rc;
-		elseif state_switch_pwm > 1800 and state_switch_pwm < 2200 then
-		data = get_vtx_channel(vtx_freq_for_third_value_rc, frequency_map);
-		current_freq = vtx_freq_for_third_value_rc;
-		else
-		gcs:send_text(0, "PWM out of expected range");
-		end;
+			current_freq = vtx_freq_for_first_value_rc;
+			set_vtx_channel(current_freq);
 
-		if data then
-			set_vtx_channel(data[1], data[2], current_freq);
+		elseif state_switch_pwm > 1300 and state_switch_pwm < 1801 then
+			current_freq = vtx_freq_for_second_value_rc;
+			set_vtx_channel(current_freq);
+
+		elseif state_switch_pwm > 1800 and state_switch_pwm < 2200 then
+			current_freq = vtx_freq_for_third_value_rc;
+			set_vtx_channel(current_freq);
+	
 		else
-			gcs:send_text(0, "Error: Frequency not found in frequency_map for selected range");
+			gcs:send_text(0, "PWM out of expected range");
 		end;
 	end
 	RC_state = state_switch_pwm;
