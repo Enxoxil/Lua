c3d18f956f9edfcd5e4abb66df75ce337ebb696c
Yehor Haponov <egoriks1994@gmail.com>
2024-12-09 13:32:54 +0200

    Func: Add auto detect numbers of channel, add param table, system of initialize

--- change_vtx_channel/change_vtx_channel_beta.lua
+++ change_vtx_channel/change_vtx_channel_beta.lua
@@ -1,6 +1,6 @@
 --
 -- @component change_vtx_channel ver.2
--- @description Change VTX channel for MILELRS with slider
+-- @description Change VTX channel for MILELRS with auto detection of the number of channels
 -- @Author: @alex_jackson.01 (signal)
 -- For To use with MILELRS a couple of steps are required.
 -- 1) Specify the Channel (aux) that will switch vtx, in parameter SCR_USER6
@@ -9,75 +9,129 @@
 -- @end
 --
 
+-- consts START --
 local TABLE_KEY = 11
-local TABLE_PREFIX = "VTX_CHANNEL"
-local LOOP_INTERVAL = 1000
-local isAddParamTable = false
-local isCheckParamTable = false
+local TABLE_PREFIX = "VTX_CHANGE"
+local LOOP_INTERVAL = 500
+local VTX_FREQ = "VTX_FREQ"
+local RC_RANGE = 1000
+-- consts END --
 
+-- var START --
+local current_freq = 0
+local pref_freq = 0
+local isAddParamTable = false
+local isInit = false
 local PARAMS = {
-	ENABLE = "ENABLE",
-	CHANNEL = "CHANNEL",
-	FREQUENCIES = {
+	VTX_CHANGE_ENABLE = "VTX_CHANGE_ENABLE",
+	VTX_CHANNEL_RC = "VTX_CHANGE_RC",
+	FREQS = {
 		"FREQ1",
 		"FREQ2",
 		"FREQ3",
 		"FREQ4",
 		"FREQ5",
-		"FREQ6"
+		"FREQ6",
+		"FREQ7",
+		"FREQ8"
 	}
 }
+-- var END --
 
+-- main START --
 local function loop()
-	local enable = get_param(PARAMS.ENABLE) or 0
+	local count = 0
+	local enable = get_param(PARAMS.VTX_CHANGE_ENABLE) or 0
 
-	if enable ~= 1 then
-		return loop, LOOP_INTERVAL_MS
+	if enable == 0 and count < 10 then
+		gcs:send_text(6, "VTX control disabled")
+		count = count + 1
+		return loop, LOOP_INTERVAL
 	end
 
-	local channel = get_param(PARAMS.CHANNEL) or nil
+	local RC_channel = get_param(PARAMS.VTX_CHANNEL_RC) or nil
 
-	if channel == nil or channel < 5 then
-		return loop, LOOP_INTERVAL_MS
+	if RC_channel == nil or RC_channel < 5 then
+		gcs:send_text(6, "VTX channel not found")
+		return loop, LOOP_INTERVAL
 	end
 
-	local channelValue = rc:get_pwm(channel)
-	if channelValue == 0 then
-		return loop, LOOP_INTERVAL_MS
+	local RC_channel_value = rc:get_pwm(RC_channel)
+	if RC_channel_value == 0 then
+		return loop, LOOP_INTERVAL
 	end
 
-	-- Получаем доступные частоты
 	local frequencies = {}
-	for _, paramName in ipairs(PARAMS.FREQUENCIES) do
+	for _, paramName in ipairs(PARAMS.FREQS) do
 		local freq = get_param(paramName) or 0
 		if freq > 1 then
 			table.insert(frequencies, freq)
 		end
 	end
 
-	-- Проверяем, есть ли доступные частоты
 	if #frequencies == 0 then
-		return loop, LOOP_INTERVAL_MS
+		return loop, LOOP_INTERVAL
 	end
 
-	-- Разделяем диапазон на количество частот
-	local rangeStep = 800 / #frequencies -- Диапазон PWM от 1100 до 1900 = 800
+	local rangeStep = RC_RANGE / #frequencies
 
 	for i, freq in ipairs(frequencies) do
-		local lowerBound = 1100 + (i - 1) * rangeStep
+		local lowerBound = 1000 + (i - 1) * rangeStep
 		local upperBound = lowerBound + rangeStep
 
-		if channelValue > lowerBound and channelValue <= upperBound then
-			param:set(VTX_CUSTOM_FREQ, freq)
+		if RC_channel_value > lowerBound and RC_channel_value <= upperBound then
+			param:set(VTX_FREQ, freq)
 			current_freq = freq
 			break
 		end
 	end
 
 	if current_freq ~= pref_freq then
-		gcs:send_text(6, "VTX freq: " .. current_freq)
+		gcs:send_text(6, "Current VTX freq: " .. current_freq)
 		pref_freq = current_freq
 	end
 
-	return loop, LOOP_INTERVAL_MS
+	return loop, LOOP_INTERVAL
 end
+
+-- main END --
+
+-- init START --
+local function init()
+	if not isAddParamTable then
+		gcs:send_text(6, "Initialize VTX control")
+
+		assert(param:add_table(TABLE_KEY, TABLE_PREFIX, 10), "The parameter table wasn`t created")
+		param:add_param(TABLE_KEY, 1, PARAMS.VTX_CHANGE_ENABLE, 0)
+		param:add_param(TABLE_KEY, 2, PARAMS.VTX_CHANNEL_RC, 0)
+		param:add_param(TABLE_KEY, 3, PARAMS.FREQ1, 0)
+		param:add_param(TABLE_KEY, 4, PARAMS.FREQ2, 0)
+		param:add_param(TABLE_KEY, 5, PARAMS.FREQ3, 0)
+		param:add_param(TABLE_KEY, 6, PARAMS.FREQ4, 0)
+		param:add_param(TABLE_KEY, 7, PARAMS.FREQ5, 0)
+		param:add_param(TABLE_KEY, 8, PARAMS.FREQ6, 0)
+		param:add_param(TABLE_KEY, 9, PARAMS.FREQ7, 0)
+		param:add_param(TABLE_KEY, 10, PARAMS.FREQ8, 0)
+
+		isAddParamTable = true
+
+		return init, 1000
+	end
+
+	if (isAddParamTable) then
+		gcs:send_text(6, "Set init freq")
+		local init_freq = get_param(PARAMS.FREQ1) or 0
+		param:set(VTX_FREQ, init_freq)
+
+		isInit = true
+		return init, 1000
+	end
+
+	if (isInit) then
+		return loop, 1000
+	end
+end
+
+return init, 2000
+
+-- init END --
