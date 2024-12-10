dd672a2a3bd747c88e79c9bef588f78f8a7bb42a
Yehor Haponov <egoriks1994@gmail.com>
2024-12-10 19:26:30 +0200

    TODO: init range rc

--- change_vtx_channel/change_vtx_channel_beta.lua
+++ change_vtx_channel/change_vtx_channel_beta.lua
@@ -14,8 +14,8 @@
 --
 
 -- consts START --
-local TABLE_KEY = 11
-local TABLE_PREFIX = "VTX_CHANGE"
+local TABLE_KEY = 51
+local TABLE_PREFIX = "VTX_"
 local LOOP_INTERVAL = 500
 local VTX_FREQ = "VTX_FREQ"
 local RC_RANGE = 1000
@@ -33,9 +33,12 @@ local range_step = 0
 local enable = 0
 local is_enable = false
 local is_init_RC_channel = false
+local is_init = false
+local lower_bound = 0
+local upper_bound = 0
 local PARAMS = {
-	VTX_CHANGE_ENABLE = "VTX_CHANGE_ENABLE",
-	VTX_CHANNEL_RC = "VTX_CHANGE_RC",
+	CHANGE_ENABLE = "CHANGE_EN",
+	CHANNEL_RC = "CHANNEL_RC",
 	FREQS = {
 		"FREQ1",
 		"FREQ2",
@@ -52,15 +55,9 @@ local PARAMS = {
 -- main START --
 local function loop()
 	RC_channel_value = rc:get_pwm(RC_channel)
-	if RC_channel_value == 0 then
-		return loop, LOOP_INTERVAL
-	end
 
-	for i, freq in ipairs(frequencies) do
-		local lowerBound = 1000 + (i - 1) * range_step
-		local upperBound = lowerBound + range_step
-
-		if RC_channel_value > lowerBound and RC_channel_value <= upperBound then
+	for _, freq in ipairs(frequencies) do
+		if RC_channel_value > lower_bound and RC_channel_value <= upper_bound then
 			param:set(VTX_FREQ, freq)
 			current_freq = freq
 			break
@@ -86,41 +83,43 @@ local function init()
 		gcs:send_text(6, "Initialize VTX control")
 
 		assert(param:add_table(TABLE_KEY, TABLE_PREFIX, 10), "The parameter table wasn`t created")
-		param:add_param(TABLE_KEY, 1, PARAMS.VTX_CHANGE_ENABLE, 0)
-		param:add_param(TABLE_KEY, 2, PARAMS.VTX_CHANNEL_RC, 0)
-		param:add_param(TABLE_KEY, 3, PARAMS.FREQ1, 0)
-		param:add_param(TABLE_KEY, 4, PARAMS.FREQ2, 0)
-		param:add_param(TABLE_KEY, 5, PARAMS.FREQ3, 0)
-		param:add_param(TABLE_KEY, 6, PARAMS.FREQ4, 0)
-		param:add_param(TABLE_KEY, 7, PARAMS.FREQ5, 0)
-		param:add_param(TABLE_KEY, 8, PARAMS.FREQ6, 0)
-		param:add_param(TABLE_KEY, 9, PARAMS.FREQ7, 0)
-		param:add_param(TABLE_KEY, 10, PARAMS.FREQ8, 0)
+		param:add_param(TABLE_KEY, 1, PARAMS.CHANGE_ENABLE, 0)
+		param:add_param(TABLE_KEY, 2, PARAMS.CHANNEL_RC, 0)
+		param:add_param(TABLE_KEY, 3, PARAMS.FREQS[1], 0)
+		param:add_param(TABLE_KEY, 4, PARAMS.FREQS[2], 0)
+		param:add_param(TABLE_KEY, 5, PARAMS.FREQS[3], 0)
+		param:add_param(TABLE_KEY, 6, PARAMS.FREQS[4], 0)
+		param:add_param(TABLE_KEY, 7, PARAMS.FREQS[5], 0)
+		param:add_param(TABLE_KEY, 8, PARAMS.FREQS[6], 0)
+		param:add_param(TABLE_KEY, 9, PARAMS.FREQS[7], 0)
+		param:add_param(TABLE_KEY, 10, PARAMS.FREQS[8], 0)
 
 		is_add_param_table = true
 
-		return init, 2000
+		return init, 1000
 	end
 
 	-- check enable vtx channel change mode
 	if not is_enable then
-		enable = get_param(PARAMS.VTX_CHANGE_ENABLE) or 0
+		gcs:send_text(6, "Check enable vtx channel change mode")
+		local enable = param:get(TABLE_PREFIX .. PARAMS.CHANGE_ENABLE) or 0
+		gcs:send_text(6, "VTX control enable: " .. enable)
 
 		if enable == 0 and count < 10 then
 			count = count + 1
 			return init, 1000
-		else if enable == 0 and count >= 10 then
+		elseif enable == 0 and count > 10 then
 			gcs:send_text(6, "VTX control disabled")
 		end
-		
+
 		is_enable = true
 		return init, 1000
 	end
 
 	-- set init freq
-	if (is_add_param_table and is_enable) then
-		gcs:send_text(6, "Set init freq")
-		local init_freq = get_param(PARAMS.FREQ1) or 0
+	if (is_add_param_table and is_enable) and not is_init_vtx_freq then
+		local init_freq = param:get(TABLE_PREFIX .. PARAMS.FREQS[1]) or 1000
+		gcs:send_text(6, "Set init freq" .. init_freq)
 		param:set(VTX_FREQ, init_freq)
 
 		is_init_vtx_freq = true
@@ -128,8 +127,9 @@ local function init()
 	end
 
 	-- set RC channel
-	if (is_init_vtx_freq) then
-		RC_channel = get_param(PARAMS.VTX_CHANNEL_RC) or nil
+	if not is_init_RC_channel then
+		gcs:send_text(6, "Set RC channel")
+		RC_channel = param:get(TABLE_PREFIX .. PARAMS.CHANNEL_RC) or nil
 		if RC_channel == nil or RC_channel < 5 then
 			gcs:send_text(6, "VTX channel not found")
 			return init, 1000
@@ -138,9 +138,10 @@ local function init()
 		return init, 1000
 	end
 
-	if (is_init_RC_channel) then
+	if not is_init then
+		gcs:send_text(6, "Set RC range")
 		for _, paramName in ipairs(PARAMS.FREQS) do
-			local freq = get_param(paramName) or 0
+			local freq = param:get(TABLE_PREFIX .. paramName) or 0
 			if freq > 1 then
 				table.insert(frequencies, freq)
 			end
@@ -150,9 +151,12 @@ local function init()
 			return init, 1000
 		end
 
+		lower_bound = 1000 + (i - 1) * range_step
+		upper_bound = lower_bound + range_step
 		range_step = RC_RANGE / #frequencies
+		is_init = true
+		return loop, LOOP_INTERVAL
 	end
-	return loop, LOOP_INTERVAL
 end
 return init, 2000
 
